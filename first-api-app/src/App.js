28e95e89ccf17b5a4df39bc1ee07cc09cc146904
Amjad Alhouseini <amjadm2008@gmail.com>
2023-06-16 15:41:56 +0300

    Fetch some api and search box

--- first-api-app/src/App.js
+++ first-api-app/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 function App() {
+  const [users, setUsers] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const getUsers = async () => {
+    const response = await fetch("https://dummyjson.com/users").then(
+      (response) => response.json()
+    );
+    setUsers(response.users);
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+  console.log("users :>> ", users);
+  const updateSearchText = (e) => {
+    setSearchText(e.target.value);
+  };
+  console.log("searchText :>> ", searchText);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="mt-3">
+        <input
+          type="text"
+          onChange={updateSearchText}
+          className="form-control"
+          placeholder="Search..."
+        />
+      </div>
+      <ul className="list-group mt-3">
+        {users
+          .filter(
+            (user) =>
+              searchText == "" ||
+              user.firstName.match(searchText) ||
+              user.lastName.match(searchText)
+          )
+          .map((user, index) => {
+            return (
+              <li key={index} className="list-group-item">
+                <img src={user.image} style={{ width: "50px" }} />
+                {user.firstName} {user.lastName}
+              </li>
+            );
+          })}
+      </ul>
     </div>
   );
 }
