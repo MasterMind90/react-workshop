35f3d85971605b4fed95600a33153f26498b1e74
Amjad Alhouseini <amjadm2008@gmail.com>
2023-06-16 14:58:54 +0300

    Add winner functionality

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -6,25 +6,56 @@ import { useState } from "react";
 function App() {
   const [cells, setCells] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(true);
+  const [winner, setWinner] = useState(null);
   console.log("cells :>> ", cells);
-
+  // [0, 1, 2, 3, 4, 5, 6, 7, 8]
   const checkWinner = (cells) => {
     for (let i = 0; i < 3; i++) {
       let ok = true;
       for (let j = 0; j < 3; j++) {
         let index = i * 3 + j;
         let base = i * 3;
+        if (!cells[index] || cells[index] !== cells[base]) {
+          ok = false;
+          break;
+        }
+      }
+      if (ok) {
+        console.log("test1");
+        if (turn) return "X";
+        return "O";
+      }
+      ok = true;
+      for (let j = 0; j < 3; j++) {
+        let index = j * 3 + i;
+        let base = i;
         if (!cells[index] || cells[index] != cells[base]) {
           ok = false;
           break;
         }
       }
       if (ok) {
-        if (cells[i * 3] == "X") return "X";
+        console.log("test2");
+        if (turn) return "X";
         return "O";
       }
     }
-    return "Draw";
+    let draw = true;
+    for (let i = 0; i < cells.length; i++) {
+      if (cells[i]) continue;
+      draw = false;
+    }
+    if (cells[0] && cells[0] == cells[4] && cells[4] == cells[8]) {
+      console.log("test3");
+      if (turn) return "X";
+      return "O";
+    }
+    if (cells[2] && cells[2] == cells[4] && cells[4] == cells[6]) {
+      console.log("test4");
+      if (turn) return "X";
+      return "O";
+    }
+    return draw ? "Draw" : "None";
   };
 
   const handleCellClick = (index) => {
@@ -33,41 +64,45 @@ function App() {
     cur[index] = turn ? "X" : "O";
     setTurn(!turn);
     setCells(cur);
-    console.log("checkWinner() :>> ", checkWinner(cur));
+    let status = checkWinner(cur);
+    if (status == "O" || status == "X" || status == "Draw") {
+      setWinner(status);
+    }
   };
   return (
     <div className="container">
       <div>
+        <h1>{winner ? `Winner is ${winner}` : ``}</h1>
         <div className="row">
-          <div className="cell" onClick={() => handleCellClick(0)}>
+          <div className="cell" onClick={() => !winner && handleCellClick(0)}>
             {cells[0] ? cells[0] : ""}
           </div>
-          <div className="cell" onClick={() => handleCellClick(1)}>
+          <div className="cell" onClick={() => !winner && handleCellClick(1)}>
             {cells[1] ? cells[1] : ""}
           </div>
-          <div className="cell" onClick={() => handleCellClick(2)}>
+          <div className="cell" onClick={() => !winner && handleCellClick(2)}>
             {cells[2] ? cells[2] : ""}
           </div>
         </div>
         <div className="row">
-          <div className="cell" onClick={() => handleCellClick(3)}>
+          <div className="cell" onClick={() => !winner && handleCellClick(3)}>
             {cells[3] ? cells[3] : ""}
           </div>
-          <div className="cell" onClick={() => handleCellClick(4)}>
+          <div className="cell" onClick={() => !winner && handleCellClick(4)}>
             {cells[4] ? cells[4] : ""}
           </div>
-          <div className="cell" onClick={() => handleCellClick(5)}>
+          <div className="cell" onClick={() => !winner && handleCellClick(5)}>
             {cells[5] ? cells[5] : ""}
           </div>
         </div>
         <div className="row">
-          <div className="cell" onClick={() => handleCellClick(6)}>
+          <div className="cell" onClick={() => !winner && handleCellClick(6)}>
             {cells[6] ? cells[6] : ""}
           </div>
-          <div className="cell" onClick={() => handleCellClick(7)}>
+          <div className="cell" onClick={() => !winner && handleCellClick(7)}>
             {cells[7] ? cells[7] : ""}
           </div>
-          <div className="cell" onClick={() => handleCellClick(8)}>
+          <div className="cell" onClick={() => !winner && handleCellClick(8)}>
             {cells[8] ? cells[8] : ""}
           </div>
         </div>
